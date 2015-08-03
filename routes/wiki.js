42c913e360c1e88d218706ce9968302795d7e0f8
yuzhencheng <yuzhencheng@focoon>
2015-08-03 09:59:18 +0800

    能用了

--- routes/wiki.js
+++ routes/wiki.js
@@ -11,7 +11,7 @@ fs.readFile(path.join(__dirname, 'pad.html'),function(err,data){
 
 router.all("/*.html", function(request, response, next) {
 
-    console.log(request.params[0]);
+  //  console.log(request.params[0]);
     var filename=path.join(__dirname,'..',"public", request.params[0]+".html");
     if( fs.existsSync(filename)){
 
@@ -26,6 +26,7 @@ router.all("/*.html", function(request, response, next) {
             //next();
         });
     }else {
+        console.log(filename+"文件不存在");
         next();//存在就用原来的方式
     };
 
@@ -51,15 +52,104 @@ router.use("/wikiAdd", function(request, response, next) {
 
     var newfilename=path.join(__dirname,'..',"public",request.body.filename);
 
-    fs.writeFile(newfilename,"空白内容",function(err){
-        if(err)
-            console.log(err+request.body.filename);
-        response.redirect("/"+request.body.filename);
-    });
+    mkdirsSync(request.body.filename,0777);
+    if(!fs.existsSync(newfilename))
+        fs.writeFile(newfilename,"空白内容",function(err){
+            if(err)
+                console.log(err+"新建文件出错"+newfilename);
 
+           // response.location("/"+request.body.filename);
+        });
+   // response.location("/"+request.body.filename);
+   // response.end();
     //   next();
     // console.log(request.body.filename,request.body.wikiContent);
+    response.set({
+        'Location': "/"+request.body.filename
+    });
+    response.end();
+});
+
+
+router.use("/wikiList", function(request, response, next) {
+
+    var filetree=new Object();
+
+    //process.chdir("../public");
+//console.log(geFileList("."));
+    response.send( JSON.stringify(geFileList( __dirname+"/../public") ));
+
+
 
 });
+function geFileList(path)
+{
+    var filesList =new Object();
+    readFile(path,filesList);
+    return filesList;
+}
+
+//遍历读取文件
+function readFile(path,filesList)
+{
+    files = fs.readdirSync(path);//需要用到同步读取
+    files.forEach(walk);
+    function walk(file)
+    {
+        states = fs.statSync(path+'/'+file);
+        if(states.isDirectory())
+        {
+        if(
+         file=="css"
+        ||file=="js"
+        ||file=="ckeditor"
+        ||file=="fonts") return;
+            filesList[file]=new Object();//目录节点
+            readFile(path+'/'+file,filesList[file]);
+        }
+        else
+        {
+            //filesList[file]
+            //创建一个对象保存信息
+           // var obj = new Object();
+           // obj.size = states.size;//文件大小，以字节为单位
+           // obj.name = file;//文件名
+         //   obj.path = path+'/'+file; //文件绝对路径
+           if( !filesList.filename  ) filesList.filename=[];
+            filesList.filename.push(file);
+        }
+    }
+}
+
+function mkdirsSync(dirpath, mode) {
+
+    process.chdir(path.join(__dirname,"..","public"));
+    if (!fs.existsSync(dirpath)) {
+        var pathtmp;
+       // var count;
+        dirpath.split(path.sep).forEach(function(dirname) {
+            if(dirname.indexOf(".html") != -1
+            ||dirname.indexOf(".htm") != -1) return;
+            if(dirname=="") return;
+            if (pathtmp) {
+                pathtmp = path.join(pathtmp, dirname);
+            }
+            else {
+                pathtmp = dirname;
+            }
+            if (!fs.existsSync(pathtmp)) {
+                if (!fs.mkdirSync(pathtmp, mode)) {
+                    return false;
+                }
+            }
+        });
+    }
+    return true;
+}
+//
+//process.chdir("../public");
+//console.log(geFileList("../public"));
+
+
 
 module.exports = router;
