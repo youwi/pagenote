356e62f80e2a28ea4da33e3a7e6be473d340ce3e
yuzhencheng <yuzhencheng@focoon>
2015-08-03 13:44:23 +0800

    加入limit

--- app.js
+++ app.js
@@ -18,11 +18,12 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-
+//app.use(bodyParser({limit: '50mb'}));
+//app.use(express.limit(100000000));
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 app.use('/', wiki);
