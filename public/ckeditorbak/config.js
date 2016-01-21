14da8ea9d9c91631acd530b5db4f9406460bbd2f
yuzhencheng <yuzhencheng@focoon>
2016-01-21 13:23:01 +0800

    ok

---
+++ public/ckeditorbak/config.js
@@ -0,0 +1,18 @@
+﻿/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,clipboard,button,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,div,resize,toolbar,elementspath,enterkey,entities,popup,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,undo,wsc,lineutils,widget,widgetbootstrap,btgrid,glyphicons,btbutton,bbcode,base64image,notification,autosave,cavacnote,chart,bt_table,btquicktable,footnotes,pbckcode,codesnippet,xml,ajax,codesnippetgeshi,codeTag,widgetcommon,ccmsconfighelper,confighelper,cssanim,divarea,format_buttons,floating-tools,imagepaste,imageresize,basewidget,layoutmanager';
+	config.skin = 'moono';
+	// %REMOVE_END%
+	//bootstrapVisibility
+
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
